cc8054ddd68f48ebe5120494aacdca8bc5510cf3
unknown <msaad9124@gmail.com>
2019-12-03 11:22:16 +0500

    assignment 24

--- Javascript/es6/assignment 24/js/app.js
+++ Javascript/es6/assignment 24/js/app.js
@@ -1,23 +1,34 @@
-let api ='https://jsonplaceholder.typicode.com/albums';
+// let api ='https://jsonplaceholder.typicode.com/albums';
 
-fetch(api)
-.then(res => res.json())
-.then(doc => console.log(doc))
-.catch(err => console.log(err))
+// fetch(api)
+// .then(res => res.json())
+// .then(doc => console.log(doc))
+// .catch(err => console.log(err))
 
 
 //default parameter
 // let num1 = prompt('first number')
 // let num2 = prompt('second number')
+// para =(num1 = 3, num2 = 4) => num1*num2
+// console.log(para())
 
-// function para(5,6){
-//     return  num + num
+// let arr1 = [1,2,3,4,5,6]
+// let arr2 =  [2,3,...arr1,4,5,8] 
+// console.log(' SPREAD OPERATORS' , arr2)
+
+// console.log('ARRAY REVERSE' , arr2.reverse())
+
+//query selector
+// let wrapper = document.querySelector('#mainWrapper')
+// console.log(wrapper)
+
+//map object
+// let obj = {
+//     name : ['ali', "ahmed" , 'sameer'],
+//     courses : 'Computer',
+//     age : [20,34,65] 
 // }
-// console.log(para(num1,num2))
-
-let arr1 = [1,2,3,4,5,6]
-let arr2 =  [2,3,...arr1,4,5,8] 
-console.log(' SPREAD OPERATORS' , arr2)
-
-console.log('ARRAY REVERSE' , arr2.reverse())
-
+// let abc =  obj.map(val =>{
+//    return val
+// })
+// console.log(abc)
